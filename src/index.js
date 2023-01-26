908ffaed96e93d866affb9874a257f29f0943236
Iryna <Iryna@MacBook-Air-MacBook.local>
2023-01-26 22:43:59 +0200

    upload project ice cream

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './js/readmore'
+import './js/franchise'
+import './js/ingredients'
+import './js/ourlocation'
+import './js/slide'
+import './js/heromenu'
---
+++ src/js/heromenu.js
@@ -0,0 +1,72 @@
+// MOBILE MENU
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    menu: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
+  };
+
+  refs.openMenuBtn.addEventListener("click", toggleMenu);
+  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle("is-hidden");
+    refs.body.classList.remove("no-scroll");
+  }
+})();
+
+// Counter HERO
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 2000;
+
+valueDisplays.forEach((valueDisplay) => {
+  let startValue = 0;
+  let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+  let duration = Math.floor(interval / endValue);
+  let counter = setInterval(function () {
+    startValue += 1;
+    valueDisplay.textContent = startValue;
+    if (startValue == endValue) {
+      clearInterval(counter);
+    }
+
+  }, duration);
+});
+
+
+////CURSOR
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+})
+
+document.addEventListener('click', () => {
+  cursor.classList.add("expand");
+
+  setTimeout(() => {
+    cursor.classList.remove("expand");
+  }, 500)
+})
+
+
+////modal window
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-hero-open]"),
+//     closeModalBtn: document.querySelector("[data-hero-close]"),
+//     modal: document.querySelector("[data-hero]"),
+//     body: document.querySelector("body"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//     refs.body.classList.remove("no-scroll");
+//   }
+// })();
